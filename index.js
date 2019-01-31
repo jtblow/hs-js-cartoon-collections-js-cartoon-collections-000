cdc78f16bc7e3b1163faf4836601715a20436722
creative-zombie-4458 <jtbartlow@gmail.com>
2019-01-31 00:04:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 function dwarfRollCall(dwarves) {
+const arr = [];
+    for (let i = 0; i < dwarves.length; i++) {
+     arr.push(`${i+1}. ${dwarves[i]} `); }
+     return arr.join('')
 }
 
-function summonCaptainPlanet(planeteerCalls){
+function summonCaptainPlanet(planeteerCalls) { 
+const arr = [];
+ for (let i = 0; i < planeteerCalls.length; i++) {
+  arr.push(`${planeteerCalls[i].toUpperCase()}!`)}
+  return arr
 }
 
 function longPlaneteerCalls(words) {
+var longCalls = words.filter(word => word.length > 4);
+if (longCalls.length > 0) {
+  return true;
+} else {
+  return false
+}
 }
 
-function findTheCheese (foods) {
+function findTheCheese (foods){
+const cheese = [];
+
+var goodCheese = foods.find(food => food === ('cheddar' || 'gouda' || 'camembert')); 
+
+if (goodCheese === undefined) {
+return 'no cheese!'
+} else { 
+  return goodCheese
 }
+}
+
